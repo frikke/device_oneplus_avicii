950fb587763ba936b9f3dbd404e62973fff942e9
Harshit Jain <god@hyper-labs.tech>
2020-09-13 21:25:23 +0530

    [TEMP] avicii: Disable recue party by default

--- product_props.mk
+++ product_props.mk
@@ -6,3 +6,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
 endif
+
+# Disable Rescue Party on userdebug & eng build
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+endif
